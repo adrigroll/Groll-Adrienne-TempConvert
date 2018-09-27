4c25c499ea577a8f86b84ded452901f038b69e8a
theag <theag@DESKTOP-GU7KQC8>
2018-09-26 23:15:28 -0600

    9/25/2018 - created individual assignment for lesson 2

---
+++ ConvertTemp/src/converttemp/ConvertTemp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converttemp;
+import java.util.Scanner;
+/**
+ *
+ * @author theag
+ */
+public class ConvertTemp {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print(" Enter the temperature : ");
+        Scanner keyboard = new Scanner(System.in);
+        int num = Integer.parseInt(keyboard.next()) ;
+        String temp = keyboard.next();
+            //Calc Fahrenheit
+            double degreeF = (9 * (num) / 5 ) + 32;
+            //Output the result
+        System.out.print( " The temperature is :  " +degreeF + " F");
+        }
+    }
+    
